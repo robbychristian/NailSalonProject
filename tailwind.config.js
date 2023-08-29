a9c714edf92c7f9138e365cdbd972fe4def72094
tinemanabs <christinemmanabat@gmail.com>
2023-08-30 00:22:55 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,7 +10,8 @@ module.exports = {
             colors: {
                 'pink': '#f9e2e1',
                 'dark-pink': '#f8c5c5',
-                'darker-pink': '#d3587b'
+                'darker-pink': '#d3587b',
+                'darker-pink-90': '#d3587be6'
             }
         },
     },
