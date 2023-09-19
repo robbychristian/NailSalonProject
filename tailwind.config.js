2741083670da6da9fe4292c813f18dfe624a150e
hwlaya <main.wayne22@gmail.com>
2023-09-19 16:16:36 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,11 +8,11 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'pink': '#f9e2e1',
-                'dark-pink': '#f8c5c5',
-                'darker-pink': '#d3587b',
-                'darker-pink-90': '#d3587be6'
-            }
+                pink: "#f9e2e1",
+                "dark-pink": "#f8c5c5",
+                "darker-pink": "#d3587b",
+                "darker-pink-90": "#d3587be6",
+            },
         },
     },
     plugins: [],
