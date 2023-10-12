090df6a3a949d4a710f1a9d68667f665838fd38b
tinemanabs <christinemmanabat@gmail.com>
2023-10-12 08:27:16 +0800

    Create index.blade.php

---
+++ resources/views/modules/dashboard/index.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.main')
+
+@section('title', '| Dashboard')
+@section('content')
+    <section>
+        <div class="px-6 py-8 mx-auto lg:py-0">
+            <div class="flex items-center justify-between mb-5 mt-3">
+                <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl">
+                    Dashboard
+                </h1>
+            </div>
+
+
+            <div id="dashboard-calendar"></div>
+        </div>
+    </section>
+@endsection
